81d231790cd31d8373da2b02295b5ae344ab75f2
Tai Truong V <tai.truong@monstar-lab.com>
2021-07-01 16:46:07 +0700

    add icon and handle status

--- src/components/UserRow.js
+++ src/components/UserRow.js
@@ -8,13 +8,29 @@ class UserRow extends Component {
         <td>{user.id}</td>
         <td>{user.email}</td>
         <td>{user.country}</td>
-        <td>{user.gender === "0" ? "Male" : "Female"}</td>
-        <td>{<input type="checkbox" name="status" checked={user.status} />}</td>
+        <td>{user.gender ? (user.gender === "0" ? "Male" : "Female") : ""}</td>
+        <td>
+          <input
+            type="checkbox"
+            name="status"
+            checked={user.status}
+            onChange={(event) =>
+              this.props.handleChangeStatus(user.id, event.target.checked)
+            }
+          />
+        </td>
         <td>{user.info}</td>
         <td>
-          <button className="" onClick={() => this.props.remove(user.id)}>
-            X
-          </button>
+          <i
+            className="fa fa-edit"
+            onClick={() => this.props.update(user.id)}
+          ></i>
+        </td>
+        <td>
+          <i
+            className="fa fa-trash"
+            onClick={() => this.props.remove(user.id)}
+          ></i>
         </td>
       </tr>
     );
